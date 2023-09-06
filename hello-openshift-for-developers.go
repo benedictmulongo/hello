a93c54f94de940abec85806d268021a7494943f4
Benedith Mulongo <null>
2023-09-06 23:17:06 +0000

    changes responsejk

--- hello-openshift-for-developers.go
+++ hello-openshift-for-developers.go
@@ -9,7 +9,7 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	response := os.Getenv("RESPONSE")
 	if len(response) == 0 {
-		response = "Hello Ben Mulongo"
+		response = "<<<<+Hello+Ben+Mulongo+>>>>"
 	}
 
 	fmt.Fprintln(w, response)
